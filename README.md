ef6ad888772d5477c6beaa53d89c7a82c8c47887
Haunted Bees <fench@hauntedbees.com>
2021-03-18 20:59:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# OpenRaster Export
+
+This will flatten an ORA file into a single image file, with the ability to limit the layers included in the output.
+
+## Install
+
+```shell
+npm install openraster-export
+```
+
+## Usage
+
+```js
+const OpenRasterExport = require("openraster-export");
+OpenRasterExport("C:/path/to/file.ora", {
+    includeLayers: ["layer1", "layer2"]
+}).then(b64 => ...);
+```
+
+## API
+
+### OpenRasterExport(imagepath, [options])
+
+Returns a Promise which resolves to a base64 data URI
+
+#### imagepath
+
+Type: `string`
+
+Path to an ORA file. Currently only ORA files with one `stack` and any number of layers is supported.
+
+#### options
+
+Type: `option`
+
+You can use either `includeLayers` and/or `includeRegex`, or you can use either `excludeLayers` and/or `excludeRegex`, but you cannot mix includes and excludes. If set, `excludeHidden` will take priority over any `includeLayers`/`includeRegex` rules.
+
+#### options.excludeHidden
+
+Type: `boolean`
+Default: `false`
+
+If true, all hidden layers (`layer visibility='hidden'`) will be excluded from the exported image.
+
+##### options.includeLayers
+
+Type: `string[]`
+Default: `undefined`
+
+List of layers - by name - to include in the exported image. Case sensitive.
+
+##### options.includeRegex
+
+Type: `RegExp`
+Default: `undefined`
+
+A regular expression that, if a layer name matches, will include that layer in the exported image.
+
+##### options.excludeLayers
+
+Type: `string[]`
+Default: `undefined`
+
+List of layers - by name - to exclude from the exported image. Case sensitive.
+
+##### options.excludeRegex
+
+Type: `RegExp`
+Default: `undefined`
+
+A regular expression that, if a layer name matches, will exclude from layer in the exported image.
+
+##### options.mergeImageOptions
+
+Type: `mergeImages.Options`
+Default: `undefined`
+
+[merge-image Options](https://github.com/lukechilds/merge-images#api). If you specify `Canvas` and `Image` options, your specified values will be used, otherwise a Canvas and Image will be provided.
+
+
+## Contributing
+
+Sure.
+
+## License
+
+Code is licensed under the [GNU Affero General Public License](https://www.gnu.org/licenses/agpl-3.0.en.html). I am not a lawyer, but what I *intend* for that to mean is, if you use this to generate images locally or as a dev dependency for something, have fun. If you use it as a standard dependency for your project and its code is included in your app/package/whatever, then you must distribute your work under an AGPL-compatible license.
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import * as fs from 'fs';
+import * as StreamZip from 'node-stream-zip';
+import * as parser from 'fast-xml-parser';
+import * as mergeImages from 'merge-images';
+import { Canvas, Image } from 'canvas';
+
+export interface ORAOptions {
+    excludeHidden?:boolean;
+    excludeLayers?:string[];
+    includeLayers?:string[];
+    excludeRegex?:RegExp;
+    includeRegex?:RegExp;
+    mergeImageOptions?:mergeImages.Options;
+}
+
+export interface ORALayer {
+    $src:string;
+    $name?:string;
+    $x?:number;
+    $y?:number;
+    $opacity?:number;
+    $visibility?:string;
+    $selected?:boolean;
+    //$composite-op?:string;
+}
+export interface ORAStack {
+    layer:ORALayer[];
+    $opacity:number;
+    $visibility:string;
+    //$composite-op:string;
+    $isolation:string;
+    $name:string;
+    $x:number;
+    $y:number;
+}
+export interface ORAImage {
+    stack:ORAStack;
+    $version:string;
+    $w:number;
+    $h:number;
+    $xres?:number;
+    $yres?:number;
+}
+export interface ORAFile { image:ORAImage }
+
+interface MergeInfo {
+    src:Buffer;
+    offsetX:number;
+    offsetY:number;
+}
+const OpenRasterExport = async function(filepath:string, options:ORAOptions):Promise<string> {
+    if(!fs.existsSync(filepath)) { throw new Error(`File ${filepath} does not exist.`); }
+    const zip = new StreamZip.async({ file: filepath });
+    const xmlBuffer = await zip.entryData("stack.xml");
+    const imgInfo:ORAFile = parser.parse(xmlBuffer.toString(), { ignoreAttributes: false, attributeNamePrefix: "$", parseAttributeValue: true });
+    const layers = imgInfo.image.stack.layer;
+    const layerBuffers:MergeInfo[] = [];
+    for(let i = 0; i < layers.length; i++) {
+        const name = layers[i].$name || "";
+        if(options.excludeHidden && layers[i].$visibility === "hidden") { continue; }
+        if(options.includeLayers || options.includeRegex) {
+            const matchDirect = options.includeLayers && options.includeLayers.indexOf(name) >= 0;
+            const matchRegex = options.includeRegex && options.includeRegex.test(name);
+            if(matchRegex || matchDirect) {
+                const buffer = await zip.entryData(layers[i].$src);
+                layerBuffers.push({
+                    src: buffer,
+                    offsetX: layers[i].$x || 0,
+                    offsetY: layers[i].$y || 0
+                });
+            }
+        } else {
+            const matchDirect = options.excludeLayers && options.excludeLayers.indexOf(name) >= 0;
+            const matchRegex = options.excludeRegex && options.excludeRegex.test(name);
+            if(!matchRegex && !matchDirect) {
+                const buffer = await zip.entryData(layers[i].$src);
+                layerBuffers.push({
+                    src: buffer,
+                    offsetX: layers[i].$x || 0,
+                    offsetY: layers[i].$y || 0
+                });
+            }
+        }
+    }
+    if(options.mergeImageOptions) {
+        if(!options.mergeImageOptions.Canvas) { options.mergeImageOptions.Canvas = Canvas; }
+        if(!options.mergeImageOptions.Image) { options.mergeImageOptions.Image = Image; }
+    } else {
+        options.mergeImageOptions = {
+            Canvas: Canvas,
+            Image: Image
+        }
+    }
+    return mergeImages(layerBuffers, options.mergeImageOptions);
+};
+export default OpenRasterExport;
